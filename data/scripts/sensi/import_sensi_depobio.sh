72d5431473af1a705d061fb70124bf1b8728f2bf
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2020-12-21 16:54:34 +0100

    update sensitivity script

--- data/scripts/sensi/import_sensi_depobio.sh
+++ data/scripts/sensi/import_sensi_depobio.sh
@@ -4,7 +4,7 @@ rootdir=`pwd`/../../../
 . $rootdir/config/settings.ini 
 export PGPASSWORD=$user_pg_pass
 rm /tmp/sensibles_to_inpn_20201218.csv
-cp ./sensibles_to_inpn_20201218.csv /tmp
+wget --cache=off https://geonature.fr/data/inpn/sensitivity/ref_sensibilite_21122020.csv -P /tmp
 psql -h $db_host -U $user_pg -d $db_name -c " DROP TABLE IF EXISTS gn_sensitivity.tmp_rules"
 
 psql -h $db_host -U $user_pg -d $db_name -c "
@@ -17,12 +17,13 @@ CREATE TABLE gn_sensitivity.tmp_rules (
   codage integer,
   autre text,
   cd_sl integer,
-  cd_occ_statut_biologique integer
+  cd_occ_statut_biologique integer,
+  cd_ref_v13 integer
 )
 "
 
 sudo -u postgres -s psql -d $db_name -c "
-COPY gn_sensitivity.tmp_rules FROM '/tmp/sensibles_to_inpn_20201218.csv' DELIMITER ';' CSV HEADER;
+COPY gn_sensitivity.tmp_rules FROM '/tmp/ref_sensibilite_21122020.csv' DELIMITER ';' CSV HEADER;
 "
 
 
@@ -30,13 +31,13 @@ psql -h $db_host -U $user_pg -d $db_name -c "
 
 delete from gn_sensitivity.cor_sensitivity_area ;
 delete from gn_sensitivity.cor_sensitivity_criteria;
-delete from gn_sensitivity.t_sensitivity_rules 
+delete from gn_sensitivity.t_sensitivity_rules;
 
 insert into gn_sensitivity.t_sensitivity_rules (
 cd_nom , nom_cite ,  id_nomenclature_sensitivity,  sensitivity_duration , sensitivity_territory, id_territory , date_min , date_max , "source" , active , "comments"
 )
-select t.cd_nom, t.nom_cite , ref_nomenclatures.get_id_nomenclature('SENSIBILITE', codage::varchar)  , coalesce(t.duree, 10000) , 'Département', t.cd_dept, null, null, 'Fichier Solene issu BDC', true, null
-from gn_sensitivity.tmp_rules t
+select t.cd_ref_v13, t.nom_cite , ref_nomenclatures.get_id_nomenclature('SENSIBILITE', codage::varchar)  , coalesce(t.duree, 10000) , 'Département', t.cd_dept, null, null, 'Fichier Solene issu BDC', true, null
+from gn_sensitivity.tmp_rules t;
 
 
 -- insertion dans cor_area_sensi
